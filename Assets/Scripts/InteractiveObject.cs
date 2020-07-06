83ff47aac85e02543c69f66d552a9801c96c1fbd
Jerri Ahonen <Jerri.ahonen@greenergrass.company>
2020-07-06 10:42:36 +0300

    Add particles

--- Assets/Scripts/InteractiveObject.cs
+++ Assets/Scripts/InteractiveObject.cs
@@ -8,6 +8,7 @@ using Debug = UnityEngine.Debug;
 public class InteractiveObject : MonoBehaviour
 {
 	public PlayableDirector director;
+	public List<ParticleSystem> particleSystems;
 
 	public void PerformAction()
     {
@@ -17,6 +18,13 @@ public class InteractiveObject : MonoBehaviour
 		    // TODO: Do particleSystem stuff
 		    Debug.Log($"Interacted with : {gameObject}", this);
 		    director.Play();
+		    if (particleSystems.Count > 0)
+		    {
+			    foreach (var system in particleSystems)
+			    {
+				    system.Play();
+			    }
+		    }
 	    }
 	    else
 	    {
